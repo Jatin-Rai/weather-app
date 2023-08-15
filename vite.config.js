3ee4d07517ada67c0d2595685b2cde64bef32e00
Jatin-Rai <jatinrai258@gmail.com>
2023-08-16 03:19:55 +0530

    redesign

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "build", // Output directory for the build artifacts
+    outDir: "build", // Output directory for the build artifacts.
   },
 });
